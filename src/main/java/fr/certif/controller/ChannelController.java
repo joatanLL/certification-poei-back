86b5925533084c98f10f3f21801237edbe65413e
Pierre Arquevaux <pierre.arquevaux@gmail.com>
2022-12-08 10:45:23 +0100

    Ajout date Message

--- src/main/java/fr/certif/controller/ChannelController.java
+++ src/main/java/fr/certif/controller/ChannelController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -39,6 +40,7 @@ public class ChannelController {
 	}
 
 	//LISTE CHANNEL
+	@CrossOrigin("http://localhost:4200")
 	@GetMapping(path = "/list", // path / url
 			produces = { "application/json" } // négociation de contenu / par défaut JSON
 	)
